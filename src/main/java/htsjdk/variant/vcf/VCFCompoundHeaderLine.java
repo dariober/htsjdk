9b03411daa2e974410e713f493224ac7744d89cc
dariober <dario.beraldi@gmail.com>
2018-05-12 19:23:42 +0100

    Do not throw "ID cannot contain..."

--- src/main/java/htsjdk/variant/vcf/VCFCompoundHeaderLine.java
+++ src/main/java/htsjdk/variant/vcf/VCFCompoundHeaderLine.java
@@ -209,9 +209,9 @@ public abstract class VCFCompoundHeaderLine extends VCFHeaderLine implements VCF
             throw new IllegalArgumentException(String.format("Invalid VCFCompoundHeaderLine: key=%s name=%s type=%s desc=%s lineType=%s",
                     getKey(), name, type, description, lineType));
         if (name.contains("<") || name.contains(">"))
-            throw new IllegalArgumentException("VCFHeaderLine: ID cannot contain angle brackets");
+            // throw new IllegalArgumentException("VCFHeaderLine: ID cannot contain angle brackets");
         if (name.contains("="))
-            throw new IllegalArgumentException("VCFHeaderLine: ID cannot contain an equals sign");
+            // throw new IllegalArgumentException("VCFHeaderLine: ID cannot contain an equals sign");
 
         if (type == VCFHeaderLineType.Flag && count != 0) {
             count = 0;
