9a9f4561ff27e9e7f1f244a833f89adfd58156fb
dariober <dario.beraldi@gmail.com>
2018-05-13 18:52:23 +0100

    Fix contog length

--- src/main/java/htsjdk/variant/vcf/VCFContigHeaderLine.java
+++ src/main/java/htsjdk/variant/vcf/VCFContigHeaderLine.java
@@ -78,6 +78,7 @@ public class VCFContigHeaderLine extends VCFSimpleHeaderLine {
 	public SAMSequenceRecord getSAMSequenceRecord() {
 		final String lengthString = this.getGenericFieldValue("length");
 		if (lengthString == null) {
+            lengthString= Integer.toString(Integer.MAX_VALUE);
             // throw new TribbleException("Contig " + this.getID() + " does not have a length field.");
         }
 		final SAMSequenceRecord record = new SAMSequenceRecord(this.getID(), Integer.valueOf(lengthString));
